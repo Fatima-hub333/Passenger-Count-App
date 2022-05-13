c368d74f199e74e9a6fc270606baeec63f0a3f08
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-13 15:12:17 +0500

    Add functions of JS for counting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+let saveEl = document.getElementById('save-el');
 let countEl = document.getElementById("count-el");
 let count = 0;
 
+// console.log(saveEl);
+
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
 function save() {
-  console.log(count);
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+
+  countEl.textContent = 0;
+  count = 0;
 }
 
 // let name = 42;
