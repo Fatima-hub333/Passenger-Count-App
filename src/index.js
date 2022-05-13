715edfce781dd0cc64355a2fc7c5e1d62b99a59d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-05-13 15:27:22 +0500

    Fis linter error issues

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
+/* eslint-disable no-unused-vars */
 const saveEl = document.getElementById('save-el');
 const countEl = document.getElementById('count-el');
 let count = 0;
-
-// console.log(saveEl);
 
 function increment() {
   count += 1;
@@ -12,9 +11,6 @@ function increment() {
 function save() {
   const countStr = `${count} - `;
   saveEl.textContent += countStr;
-
-  countEl.textContent = 0;
-  count = 0;
 }
 
 // let name = 42;
